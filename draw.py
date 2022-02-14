cff781874fb5995653ec406fedee129429e65bfe
Rickey Dong <rickeyd2004@gmail.com>
2022-02-14 11:01:52 -0500

    work done from class today

--- draw.py
+++ draw.py
@@ -1,4 +1,17 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    # if second pixel's x-cor is < first pixel's x-cor, swap coords
+    # so that we draw from left to right
+    if x1 < x0:
+        swap_x = x1
+        swap_y = y1
+        x1 = x0
+        y1 = y0
+        x0 = swap_x
+        y0 = swap_y
+
+    if (x1 == x0): # handling undefined slopes
+        draw_line_octant_two(x0, y0, x1, y1, screen, color)
+    else:
+        slope = (y1 - y0) / (x1 - x0)
