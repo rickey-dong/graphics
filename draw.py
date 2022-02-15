945f54b2c25625bfde0784eb47de037f18aff166
Rickey Dong <rickeyd2004@gmail.com>
2022-02-14 22:29:44 -0500

    octant one code

--- draw.py
+++ draw.py
@@ -1,5 +1,19 @@
 from display import *
 
+def draw_line_octant_one(x0, y0, x1, y1, screen, color):
+    x = x0
+    y = y0
+    A = 2(y1 - y0)
+    B = -2(x1 - x0)
+    d = A + B/2
+    while x <= x1:
+        plot(screen, color, x, y)
+        if d > 0:
+            y += 1
+            d += B
+        x += 1
+        d += A
+
 def draw_line( x0, y0, x1, y1, screen, color ):
     # if second pixel's x-cor is < first pixel's x-cor, swap coords
     # so that we draw from left to right
