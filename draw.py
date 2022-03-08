5b7c29e43f6102c8bfaebece31a0ca3c73b47639
Rickey Dong <rickeyd2004@gmail.com>
2022-03-08 16:18:51 -0500

    integerized the inputs for draw line

--- draw.py
+++ draw.py
@@ -27,7 +27,10 @@ def add_point( matrix, x, y, z=0 ):
 
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-
+    x0 = int(x0)
+    y0 = int(y0)
+    x1 = int(x1)
+    y1 = int(y1)
     #swap points if going right -> left
     if x0 > x1:
         xt = x0
