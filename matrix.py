da74b93a6e9937c4deef17fbd154552adad0a615
Rickey Dong <rickeyd2004@gmail.com>
2022-03-14 10:42:35 -0400

    made bezier curve matrix

--- matrix.py
+++ matrix.py
@@ -10,7 +10,13 @@ z0  z1  ... zn
 import math
 
 def make_bezier():
-    pass
+    bezier_curve_matrix = [
+        [-1, 3, -3, 1],
+        [3, -6, 3, 0],
+        [-3, 3, 0, 0],
+        [1, 0, 0, 0]
+    ]
+    return bezier_curve_matrix
 
 def make_hermite():
     pass
