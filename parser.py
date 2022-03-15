a100ecc49e38944cadcd81ef5869ce500fd14084
Rickey Dong <rickeyd2004@gmail.com>
2022-03-15 11:00:10 -0400

    hermite curve command:

--- parser.py
+++ parser.py
@@ -67,6 +67,10 @@ def parse_file( fname, edges, transform, screen, color ):
         elif line == 'circle':
             add_circle(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), 0.0008)
 
+        elif line == 'hermite':
+            add_curve(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]),
+            float(args[5]), float(args[6]), float(args[7]), 0.0008, 'hermite')
+
         elif line == 'scale':
             #print 'SCALE\t' + str(args)
             t = make_scale(float(args[0]), float(args[1]), float(args[2]))
