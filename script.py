c4988c1e4708de2b7a5bcffdd9066360eb94aed9
Rickey Dong <rickeyd2004@gmail.com>
2022-05-16 16:31:01 -0400

    added print outputs for easier viewing

--- script.py
+++ script.py
@@ -84,3 +84,26 @@ def run(filename):
          elif actual_command == "save":
             
          elif actual_command == "display":
+
+# HELLO
+# {'shiny_purple': ['constants', {'red': [0.3, 0.2, 0.8], 'green': [0.3, 0.0, 0.0], 'blue': [0.3, 0.2, 0.8]}], 'shiny_teal': ['constants', {'red': [0.3, 0.0, 0.0], 'green': [0.3, 0.2, 0.8], 'blue': [0.3, 0.2, 0.8]}], 'dull_yellow': ['constants', {'red': [0.3, 0.8, 0.2], 'green': [0.3, 0.8, 0.2], 'blue': [0.0, 0.0, 0.0]}], '.white': ['constants', {'red': [0.2, 0.5, 0.5], 'green': [0.2, 0.5, 0.5], 'blue': [0.2, 0.5, 0.5]}]}
+# HEY
+# {'op': 'constants', 'args': None, 'constants': 'shiny_purple'}
+# {'op': 'constants', 'args': None, 'constants': 'shiny_teal'}
+# {'op': 'constants', 'args': None, 'constants': 'dull_yellow'}
+# {'op': 'push', 'args': None}
+# {'op': 'move', 'args': [250.0, 250.0, 0.0], 'knob': None}
+# {'op': 'sphere', 'constants': 'shiny_purple', 'cs': None, 'args': [-100.0, 150.0, 0.0, 80.0]}
+# {'op': 'sphere', 'constants': 'shiny_teal', 'cs': None, 'args': [100.0, 150.0, 0.0, 80.0]}
+# {'op': 'push', 'args': None}
+# {'op': 'rotate', 'args': ['x', 45.0], 'knob': None}
+# {'op': 'rotate', 'args': ['y', 45.0], 'knob': None}
+# {'op': 'box', 'constants': None, 'cs': None, 'args': [-40.0, 40.0, 40.0, 80.0, 80.0, 80.0]}
+# {'op': 'pop', 'args': None}
+# {'op': 'push', 'args': None}
+# {'op': 'move', 'args': [0.0, -150.0, 0.0], 'knob': None}
+# {'op': 'rotate', 'args': ['x', 30.0], 'knob': None}
+# {'op': 'scale', 'args': [1.0, 1.0, 0.5], 'knob': None}
+# {'op': 'torus', 'constants': 'dull_yellow', 'cs': None, 'args': [0.0, 0.0, 0.0, 30.0, 175.0]}
+# {'op': 'display', 'args': None}
+# {'op': 'save', 'args': ['face']}
