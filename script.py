da5a11e3243a1787c8bb2e11dc54378bc10e1398
Rickey Dong <rickeyd2004@gmail.com>
2022-05-16 10:47:47 -0400

    created overall structure of code

--- script.py
+++ script.py
@@ -45,7 +45,36 @@ def run(filename):
                           'green': [0.2, 0.5, 0.5],
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
-
+    print("HELLO")
     print(symbols)
+    print("HEY")
     for command in commands:
         print(command)
+        actual_command = command['op']
+        if actual_command == "push":
+
+        elif actual_command == "pop":
+
+        elif actual_command == "move" or actual_command == "rotate" or actual_command == "scale":
+            
+            if actual_command == "move":
+            
+            elif actual_command == "rotate":
+            
+            else:
+
+        elif actual_command == "box" or actual_command == "sphere" or actual_command == "torus":
+        
+            if actual_command == "box":
+            
+            elif actual_command == "sphere":
+            
+            else:
+
+        elif actual_command == "constants":
+        
+        elif actual_command == "line":
+        
+        elif actual_command == "save":
+            
+        elif actual_command == "display":
